1efba1d17297fa476d603736d7f2759ef60cd839
NotAloneAgain <maoffru@gmail.com>
2023-12-27 10:48:49 +0300

    upd

--- Toji.Map/Handlers/MapHandlers.cs
+++ Toji.Map/Handlers/MapHandlers.cs
@@ -31,7 +31,7 @@ namespace Toji.BetterMap.Handlers
                 NetworkServer.UnSpawn(generator.GameObject);
 
                 generator.Transform.localScale *= 0.8f;
-                generator.Transform.localPosition += Vector3.up * 0.3f;
+                generator.Transform.localPosition += Vector3.up * 1.6f;
 
                 NetworkServer.Spawn(generator.GameObject);
             }
